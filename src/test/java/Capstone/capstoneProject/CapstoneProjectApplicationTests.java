8b68d74c9354a33ec0ba08148c0a55a7615d5574
yoonjeonggg <164142448+yoonjeonggg@users.noreply.github.com>
2025-10-25 03:05:27 +0900

    db설정

--- src/test/java/Capstone/capstoneProject/CapstoneProjectApplicationTests.java
+++ src/test/java/Capstone/capstoneProject/CapstoneProjectApplicationTests.java
@@ -1,13 +1,25 @@
 package Capstone.capstoneProject;
 
+import Capstone.capstoneProject.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@DataJpaTest
+@ActiveProfiles("test")
+@EntityScan(basePackages = "Capstone.capstoneProject.entity")
+@EnableJpaRepositories(basePackages = "Capstone.capstoneProject.repository")
 class CapstoneProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private UserRepository userRepository;
 
+	@Test
+	void repositoryLoads() {
+		assert userRepository != null;
+	}
 }
+
