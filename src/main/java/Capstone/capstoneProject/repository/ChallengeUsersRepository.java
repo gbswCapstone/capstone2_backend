ff5b9f068283f5dfdf673639ada913ecf1f478b0
yoonjeonggg <164142448+yoonjeonggg@users.noreply.github.com>
2025-10-27 00:45:21 +0900

    my_challenges

--- src/main/java/Capstone/capstoneProject/repository/ChallengeUsersRepository.java
+++ src/main/java/Capstone/capstoneProject/repository/ChallengeUsersRepository.java
@@ -1,9 +1,13 @@
 package Capstone.capstoneProject.repository;
 
 import Capstone.capstoneProject.entity.challenges.ChallengeUsers;
+import Capstone.capstoneProject.entity.challenges.Challenges;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ChallengeUsersRepository extends JpaRepository<ChallengeUsers, Long> {
     boolean existsByChallengeIdAndUserId(Long challengeId, Long userId);
     int countByChallengeId(Long challengeId);
+    List<ChallengeUsers> findByUserId(Long userId);
 }
