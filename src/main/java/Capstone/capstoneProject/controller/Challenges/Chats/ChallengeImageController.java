6f90a1788ff15a9d0189305e132cb773775889fa
yoonjeonggg <88yfg@naver.com>
2025-12-21 03:58:49 +0900

    notice_delete

--- src/main/java/Capstone/capstoneProject/controller/Challenges/Chats/ChallengeImageController.java
+++ src/main/java/Capstone/capstoneProject/controller/Challenges/Chats/ChallengeImageController.java
@@ -27,6 +27,7 @@ public class ChallengeImageController {
     @Operation(summary = "챌린지 채팅 이미지 전송 (WebSocket)",
             description = """
         실제 메시지 전송은 WebSocket(STOMP)을 사용합니다.
+        Headers에 roomId 넣어야합니다.
         SEND: /pub/api/challenges/chat/images
         SUBSCRIBE: /sub/challenges/chat/room/{roomId}
         """)
